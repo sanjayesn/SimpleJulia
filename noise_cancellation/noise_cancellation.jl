c70e35be46d7c73fdf9cd6e048e3df70cfd336ad
Sanjaye and Sid <sanjayesn@gmail.com>
2018-12-07 13:12:52 -0800

    Add noise cancellation project

---
+++ noise_cancellation/noise_cancellation.jl
@@ -0,0 +1,33 @@
+
+using WAV
+using Plots
+
+ntracks = 3;
+sec = 5;
+Fs = 44100; # Sampling frequency
+
+# Ambient measurements
+A = zeros(sec*Fs, ntracks); # K=3 noise samples of five sec each
+A[:,1], Fs = wavread("tsunami_cut.wav");
+A[:,2], Fs = wavread("static_cut.wav");
+A[:,3], Fs = wavread("symphony_cut.wav");
+
+# Noisy signal we want to estimate
+n, Fs = wavread("n_train.wav");
+
+# Sound to denoise:
+noisy, Fs = wavread("noisy_target.wav");
+
+plot((1:length(noisy))/Fs, noisy,  title="Noisy Signal", xlabel="Time (seconds)", label="")
+
+# Generate theta to minimize noise in signal
+theta = zeros(Fs)
+theta = A \ -n
+;
+
+# Generate denoised audio signal
+denoised = zeros(Fs)
+c = A * theta
+denoised = noisy + c
+
+plot((1:length(denoised))/Fs, denoised,  title="Denoised Signal", xlabel="Time (seconds)", label="")
